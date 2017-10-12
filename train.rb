dedf97a3e6e8635ca56bf399af2f02ed3c800823
danilpanshin <danilpanshin@gmail.com>
2017-10-12 13:37:52 +0300

    fourth try

--- train.rb
+++ train.rb
@@ -1,3 +1,27 @@
+class Station
+
+  attr_reader :stations
+
+  def initialize(name)
+    @station_name = name
+    @trains = []
+  end
+
+  def arrival_train(train)
+    @trains << train
+  end
+
+  def departure_train(train)
+    @trains.delete(train)
+  end
+
+  def list_by_type(type)
+    num = 0
+    @trains.each {|traintype| num += 1 if traintype == type}
+  end
+
+end
+
 class Route
 
   attr_reader :stations
@@ -10,17 +34,15 @@ class Route
     @stations.insert(@stations.length - 1, name)
   end
 
-  def list
-    @stations
-  end
-
-  def delete_station(name)
-    @stations.delete(name)
+  def delete_station(station)
+    @stations.delete(station)
   end
 
 end
 
 class Train
+
+  attr_reader :speed, :wagons
 
   def initialize(number, type, wagons, speed = 0)
     @number = number
@@ -31,19 +53,10 @@ class Train
 
   def accelerate(num)
     @speed += num
-    puts "скорость увеличена на #{num}км/ч"
-  end
-
-  def current_speed
-    puts "текущая скорость #{speed}км/ч"
   end
 
   def break
     @speed = 0
-  end
-
-  def wagons_amount
-    @wagons
   end
 
   def take_wagon
@@ -55,48 +68,32 @@ class Train
   end
 
   def set_route(route)
-
-    @route = route.stations
-    @index_station = 0
-    puts "Поезд находится на станции #{@route.stations[@index_station]}"
+    @route = route
+    current_station.arrival_train(self)
   end
 
   def forward
-    if @index_station == @route.stations.length
-      puts "Конечная станция"
-    else
-      @index_station += 1
-      puts "Поезд находится в #{@route.stations[@index_station]}"
-    end
+    current_station.departure_train(self)
+    @index_station += 1
+    current_station.arrival_train(self)
   end
 
   def back
-    if @index_station == 0
-      puts "конечная станция"
-    else
-      @index_station -= 1
-      puts "Поезд находится в #{@route.stations[@index_station]}"
-    end
+    current_station.departure_train(self)
+    @index_station -= 1
+    current_station.arrival_train(self)
   end
 
   def current_station
-    puts "Поезд находится в #{@route.stations[@index_station]}"
+    @route.stations[@index_station]
   end
 
   def previous_station
-    if @index_station == 0
-      puts "конечная станция"
-    else
-      puts "Поезд находился в #{@route.stations[@index_station - 1]}"
-    end
+    @route.stations[@index_station - 1]
   end
 
   def next_station
-    if @index_station == @route.stations.length
-      puts "Конечная станция"
-    else
-      puts "Поезд будет находиться в #{@route.stations[@index_station + 1]}"
-    end
+    @route.stations[@index_station + 1]
   end
 
 end
